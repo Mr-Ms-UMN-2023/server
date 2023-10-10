d98eb4bd25f25071138c3b2e0f3222121fb6477d
farrel dinarta <farrel.dinarta@student.umn.ac.id>
2023-10-10 10:04:25 +0700

    add origins to all and credentials : true

--- server.js
+++ server.js
@@ -13,7 +13,10 @@ const APP_URL = process.env.APP_URL;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cors());
+app.use(cors({
+  origin : '*',
+  credentials : true,
+}));
 app.use(fileUpload());
 
 
