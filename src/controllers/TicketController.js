ef8597c1a774644cc6a65a2bbc1a986953e18e73
farrel dinarta <farrel.dinarta@student.umn.ac.id>
2023-10-12 09:58:15 +0700

    test

--- src/controllers/TicketController.js
+++ src/controllers/TicketController.js
@@ -262,22 +262,20 @@ const paymentNotification = async (req, res) => {
 
             const renderHtml = ejs.render(html, variables);
 
-            // , (err, data) => {
+            sendEmail(email, "[ Tiket Himalaya MR & MS UMN 2023 ]", renderHtml, attachments, (err, data) => {
 
-            //   if (err) {
-            //     console.log("Error sending email : ", err.message);
-            //   }
+              if (err) {
+                console.log("Error sending email : ", err.message);
+              }
 
-            //   for (let file of attachments){
-            //     fs.unlink(file?.path, (err) => {
-            //       if (err) {
-            //         console.error(`Error deleting file: ${err}`);
-            //       } 
-            //     });
-            //   }
-            // }
-
-            sendEmail(email, "[ Tiket Himalaya MR & MS UMN 2023 ]", renderHtml, attachments);
+              for (let file of attachments){
+                fs.unlink(file?.path, (err) => {
+                  if (err) {
+                    console.error(`Error deleting file: ${err}`);
+                  } 
+                });
+              }
+            });
 
             return res.status(201).json({
               status: "SUCCESS",
