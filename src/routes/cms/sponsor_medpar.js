5845990935ccc7518e68e7ecf9cc2e856339bc87
KoongHiap <98584656+Koongh@users.noreply.github.com>
2023-10-26 23:04:17 +0700

    fix: middleware sponsor medpar

--- src/routes/cms/sponsor_medpar.js
+++ src/routes/cms/sponsor_medpar.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const SponsorMedparController = require("../../controllers/cms/SponsorMedparController");
-const Middlewares = require("../../middlewares/user")
+const Middlewares = require("../../middlewares/user");
 
-router.get("/get/sponsors", Middlewares.auth ,SponsorMedparController.getAllSponsor);
-router.get("/get/medpar", Middlewares.auth, SponsorMedparController.getAllMedpar);
+router.get("/get/sponsors", SponsorMedparController.getAllSponsor);
+router.get("/get/medpar", SponsorMedparController.getAllMedpar);
 
 module.exports = router;
