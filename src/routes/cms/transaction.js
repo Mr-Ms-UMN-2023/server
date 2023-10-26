ad5b464e71358a20fb3d68cafd4990f6364b7e67
farrel dinarta <farrel.dinarta@student.umn.ac.id>
2023-10-26 21:53:56 +0700

    feat : enable middleware for admin endpoints

--- src/routes/cms/transaction.js
+++ src/routes/cms/transaction.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const TransactionController = require("../../controllers/cms/TransactionController");
+const Middlewares = require('../../middlewares/user');
 
-router.get("/get/transaction", TransactionController.getTransaction);
+router.get("/get/transaction", Middlewares.auth, TransactionController.getTransaction);
 
 module.exports = router;
