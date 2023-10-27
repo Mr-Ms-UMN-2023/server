7626322f29589b8df0263d4019c6cf33c602b963
KoongHiap <98584656+Koongh@users.noreply.github.com>
2023-10-27 15:42:41 +0700

    fix: phoneNumber validation

--- src/helpers/data_validation.js
+++ src/helpers/data_validation.js
@@ -1,27 +1,26 @@
 const isValidEmail = (email) => {
-    const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;    
-    return pattern.test(email);
-}
+  const pattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return pattern.test(email);
+};
 
 const isValidPhoneNumber = (phoneNumber) => {
-    const pattern = /^\d{9,12}$/;
-    return pattern.test(phoneNumber);
-}
+  const pattern = /^\d{9,15}$/;
+  return pattern.test(phoneNumber);
+};
 
 const isValidName = (name) => {
-    return name.length > 0;
-}
+  return name.length > 0;
+};
 
 const isPositiveInteger = (input) => {
-    const num = parseInt(input);
-    if (!parseInt) return false;
-    return num > 0;
-}
-
+  const num = parseInt(input);
+  if (!parseInt) return false;
+  return num > 0;
+};
 
 module.exports = {
-    isValidEmail, 
-    isValidPhoneNumber,
-    isValidName,
-    isPositiveInteger
-}
+  isValidEmail,
+  isValidPhoneNumber,
+  isValidName,
+  isPositiveInteger,
+};
