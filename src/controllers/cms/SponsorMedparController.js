aa134e0c85e889dbff66fc01cfc624464db260d2
KoongHiap <98584656+Koongh@users.noreply.github.com>
2023-10-27 10:56:16 +0700

    feat: sponsor medpar cmd

--- src/controllers/cms/SponsorMedparController.js
+++ src/controllers/cms/SponsorMedparController.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 
 const SponsorMedpar = require("../../models/SponsorMedpar");
+const randomToken = require("random-token");
 
 const getAllSponsor = async (req, res) => {
   try {
@@ -38,4 +39,92 @@ const getAllMedpar = async (req, res) => {
   }
 };
 
-module.exports = { getAllMedpar, getAllSponsor };
+const sponsorDetail = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const data = await SponsorMedpar.query()
+      .where({ Sponsor_MedparID: id })
+      .first();
+    console.log("hello", id);
+    return res.status(201).json({
+      status: "SUCCESS",
+      type: "DETAIL",
+      code: 201,
+      data: data,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: "Internal Server Error : " + err.message,
+    });
+  }
+};
+
+const createData = async (req, res) => {
+  try {
+    const { type = "", img = "", nama = "", url = "", bg = false } = req.body;
+
+    const data = await SponsorMedpar.query().insert({
+      Sponsor_MedparID: randomToken(32),
+      type: Number(type),
+      src: img,
+      nama: nama,
+      url: url,
+      bg: bg,
+    });
+
+    return res.status(201).json({
+      status: "SUCCESS",
+      type: "DETAIL",
+      code: 201,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: "Internal Server Error : " + err.message,
+    });
+  }
+};
+
+const editData = async (req, res) => {
+  try {
+    const {
+      id = "",
+      type = "",
+      img = "",
+      nama = "",
+      url = "",
+      bg = false,
+    } = req.body;
+
+    console.log(req.body);
+
+    const data = await SponsorMedpar.query()
+      .where({ Sponsor_MedparID: id })
+      .update({
+        type: type,
+        src: img,
+        nama: nama,
+        url: url,
+        bg: bg,
+      });
+    return res.status(201).json({
+      status: "SUCCESS",
+      type: "DETAIL",
+      code: 201,
+    });
+  } catch (err) {
+    return res.status(500).send({
+      code: 500,
+      message: "Internal Server Error : " + err.message,
+    });
+  }
+};
+
+module.exports = {
+  getAllMedpar,
+  getAllSponsor,
+  sponsorDetail,
+  createData,
+  editData,
+};
--- src/routes/cms/sponsor_medpar.js
+++ src/routes/cms/sponsor_medpar.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
 const SponsorMedparController = require("../../controllers/cms/SponsorMedparController");
+const Middlewares = require("../../middlewares/user");
 
 router.get("/get/sponsors", SponsorMedparController.getAllSponsor);
+router.get(
+  "/get/:id",
+  //   Middlewares.auth,
+  SponsorMedparController.sponsorDetail
+);
 router.get("/get/medpar", SponsorMedparController.getAllMedpar);
-
+router.post("/put", SponsorMedparController.createData);
+router.post("/edit", SponsorMedparController.editData);
 module.exports = router;
