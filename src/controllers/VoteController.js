54c89955ba8d29c1506d211756db7669b898ed36
farrel dinarta <farrel.dinarta@student.umn.ac.id>
2023-11-12 16:23:03 +0700

    feat : vote APIs

--- src/controllers/VoteController.js
+++ src/controllers/VoteController.js
@@ -123,7 +123,44 @@ const orderVote = async (req, res) => {
 // const votePaymentNotification = async (req, res) => {
 // }
 
+const getAllFinalistVoteCount = async (req, res) => {
+    try {
+
+        const data = await knex('finalis_2023')
+        .select('finalis_2023.*')
+        .select(knex.raw('COALESCE(SUM(vote_list_2023.quantity), 0) AS total_vote'))
+        .leftJoin('vote_list_2023', function () {
+          this.on('vote_list_2023.finalis_id', '=', 'finalis_2023.id')
+            .andOn('vote_list_2023.status', '=', knex.raw('"settlement"'));
+        })
+        .groupBy('finalis_2023.id')
+
+        return res.status(200).send({
+            code: 200,
+            message: "Berhasil mendapatkan data statistik vote.",
+            data,
+        });            
+        
+    } catch (err) {
+    if (err instanceof ValidationException) {
+      return res.status(err.code).send({
+        code: err.code,
+        type: err.type,
+        message: err.message,
+        error: validationInfoList,
+      });
+    }
+    console.error(err);
+    return res.status(500).send({
+      code: 500,
+      message: "Internal Server Error : " + err.message,
+    });
+  }
+}
+
 module.exports = {
     orderVote,
+    getAllFinalistVoteCount, 
     // votePaymentNotification
+    
 }
--- src/routes/vote.js
+++ src/routes/vote.js
@@ -4,5 +4,6 @@ const TicketController = require("../controllers/TicketController");
 const VoteController = require("../controllers/VoteController");
 
 router.post("/order", VoteController.orderVote);
+router.get("/statistic", VoteController.getAllFinalistVoteCount);
 
 module.exports = router;
